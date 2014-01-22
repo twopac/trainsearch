77d509456b78c889395888ca101b2acc9883e62f
Ilya Usanov <twopac@bk.ru>
2014-01-22 13:26:00 +0400

    change .gitignor

--- rzd/views.py
+++ rzd/views.py
@@ -3,12 +3,16 @@ import urllib
 import urllib2
 import requests
 import json
+import copy
 
 import pickledb
 import chromelogger as console
 from jsonview.decorators import json_view
 from django.http import HttpResponse
 from annoying.decorators import render_to
+from datetime import datetime
+
+from scrap import get_trains
 
 
 StationsCache = pickledb.load('stations.db', False)
@@ -37,9 +41,11 @@ def cross_domain_ajax(func):
 @json_view
 def suggester(request):
     """Принимает начало названия станции. Смотрит в кэше по первым двум буквам
-    список возможных станций, если нету то спрашивает у РЖД и кэширует его.
+    список возможных станций, если нету то спрашивает у РЖД и кэширует его, попутно
+    удаляя станции повторяющиеся с одинаковим id и name, но разными key.
     Далее оставляет только станции совпадающие и по остальным представленным буквам
-    и возвращает их имя и id
+    и возвращает их имя и id.
+    В качестве кэша pickledb.
 
     """
     suggest = []
@@ -50,6 +56,13 @@ def suggester(request):
     else:
         url = 'http://pass.rzd.ru/suggester/?lang=ru&stationNamePart=%s' % firstTwoLetters
         r = requests.get(url).json()
+        l = []
+        s = set()
+        for obj in r:
+            if obj['name'] not in s:
+                l.append(obj)
+                s.add(obj['name'])
+        r = l[:]
         StationsCache.set(firstTwoLetters, r)
         StationsCache.dump()
     for st in r:
@@ -59,5 +72,18 @@ def suggester(request):
 
 @render_to('trains.html')
 def trains(request):
-    console.log(request)
-    return {'rq': request.POST.items()}
+    d = datetime.strptime(request.POST['date'], '%Y-%m-%d')
+    trains = get_trains(code0=request.POST['stationFromId'],
+                        code1=request.POST['stationToId'],
+                        date=d.strftime('%d.%m.%Y'))
+    empty = {u'Плац': '-', u'Купе': '-', u'Люкс': '-'}
+    for train in trains:
+        train['places'] = copy.deepcopy(empty)
+        train['prices'] = copy.deepcopy(empty)
+        for vagon in train['cars']:
+            console.log(vagon['type'])
+            console.log(vagon['freeSeats'])
+            train['places'][unicode(vagon['type'])] = vagon['freeSeats']
+            train['prices'][unicode(vagon['type'])] = vagon['tariff']
+    console.log(trains)
+    return {'trains': trains}
--- scrap.py
+++ scrap.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
-import urllib
-import urllib2
+import json
+import time
+
+import requests
 
 
-def request_to_site(url):
-    USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows 98)'
-    header = {'User-Agent': USER_AGENT, 'X-JAVASCRIPT-ENABLED': 'false'}
-    html = urllib2.urlopen(urllib2.Request(url, headers=header)).read()
-    return html
+base_url = """http://pass.rzd.ru/timetable/public/ru?STRUCTURE_ID=735\
+&layer_id=5371&dir=0&tfl=3&checkSeats=0&withoutSeats=y&code0={code0}&dt0={dt0}\
+&code1={code1}&dt1={dt1}"""
 
-if __name__ == '__main__':
-    print request_to_site('http://pass.rzd.ru/suggester/?lang=ru&stationNamePart=во')
+def get_trains(code0, code1, date):
+    url = base_url.format(code0=code0, code1=code1, dt0=date, dt1=date)
+    r = requests.get(url)
+    rid = json.loads(r.text)['rid']
+    JSESSIONID = r.cookies['JSESSIONID']
+    time.sleep(3)
+    url = url + '&SESSION_ID=1&rid=' + str(rid)
+    r = requests.get(url, cookies={'JSESSIONID':JSESSIONID})
+#    f = open('r.txt', 'wb')
+#    f.write(r.text.encode('utf-8'))
+#    f.close()
+    for i in xrange(0,10):
+        while True:
+            try:
+                trains = json.loads(r.text)['tp'][0]['list']
+            except:
+                continue
+            break
+    return trains
+
+#
+#if __name__ == '__main__':
+#    print get_trains()
